3f386954aed054bce2425a60775a76a125bbf437
verywelloo <verywelloo@hotmail.com>
2024-12-22 16:42:29 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,92 @@
 "use strict";
 let id = 5;
-console.log('ID:', id);
+let company = 'Google';
+let isPasswordCorrect = true;
+let x = 'Hello';
+let age;
+age = 28;
+let ids = [1, 2, 3, 4];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Tom', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill']
+];
+// Union 
+let pid;
+pid = 123;
+// Enum 
+// default console.log(Direction1.Up) = 0
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+// Object
+const user = {
+    id: 1,
+    name: 'John'
+};
+const user2 = {
+    id: 123,
+    name: 'Tom'
+};
+// Type Assertion => specific type of variable
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+const user3 = {
+    id: 1,
+    name: 'John'
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+// Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    // method
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const brad = new Person(1, 'Brad');
+const mike = new Person(2, 'Mike');
+// Subclasses
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+const emp = new Employee(3, 'Shawn', 'Developer');
+// Generics => create type variable
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4]);
+let stringArray = getArray(['1', '2', '3', '4']);
+stringArray.push('hello');
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,142 @@
 let id: number = 5
+let company: string = 'Google'
+let isPasswordCorrect: boolean = true
+let x: any = 'Hello'
+let age:number
+age = 28
 
-console.log('ID:', id)
+let ids: number[] = [1,2,3,4]
+let arr: any[] = [1,true, 'Hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Tom', true]
+// Tuple Array
+let employee: [number, string][] 
+
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill']
+]
+
+// Union 
+let pid : string | number 
+pid = 123
+
+// Enum 
+// default console.log(Direction1.Up) = 0
+enum Direction1 {
+    Up = 1, // now console.log(Direction1.Up) = 1
+    Down, // now console.log(Direction1.Down) = 2
+    Left,
+    Right,
+}
+enum Direction2 {
+    Up = 'Up', // now console.log(Direction2.Up) = 'Up'
+    Down  = 'Down', // now console.log(Direction2.Down) = 'Down'
+    Left = 'Left',
+    Right = 'Right',
+}
+
+// Object
+const user:{
+    id: number,
+    name: string
+} = {
+    id: 1,
+    name: 'John'
+}
+
+type User = {
+    id:number,
+    name:string,
+}
+const user2:User = {
+    id:123,
+    name:'Tom'
+}
+
+// Type Assertion => specific type of variable
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
+
+// Functions
+function addNum(x: number, y: number): number {
+    return x + y
+}
+
+// Void
+function log(message: string | number): void { // void : mean function that not return a value
+    console.log(message)
+}
+
+// Interfaces => setting type for a object or function
+interface UserInterface  {
+    readonly id: number, // cannot edit id property
+    name: string,
+    age?: number, // ? means okay for having age or not
+}
+
+const user3: UserInterface = {
+    id: 1,
+    name: 'John'
+}
+
+//interface Point = number | string => cannot do that 
+
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y:number):number => x+y
+const sub: MathFunc = (x: number, y:number):number => x-y
+
+
+interface PersonInterface  {
+    id: number, // cannot edit id property
+    name: string,
+    register(): string
+}
+
+// Classes
+class Person implements PersonInterface {
+    // private id: number => private means just visible in class. protected means visible in class and extended class.
+    id: number
+    name: string
+
+    constructor(id: number, name: string) {
+        this.id = id
+        this.name = name
+    }
+
+    // method
+    register() {
+        return `${this.name} is now registered`
+    }
+}
+
+const brad = new Person(1, 'Brad')
+const mike = new Person(2, 'Mike')
+
+// Subclasses
+class Employee extends Person {
+    position: string
+
+    constructor(id: number, name: string, position: string) {
+         super(id, name)
+         this.position = position
+    }
+}
+
+const emp = new Employee(3, 'Shawn', 'Developer')
+
+// Generics => create type variable
+function getArray<T>(items:T[]):T[] {
+    return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1,2,3,4])
+let stringArray = getArray<string>(['1','2','3','4'])
+
+stringArray.push('hello')
